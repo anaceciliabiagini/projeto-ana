10f98cd3ca993a9b4cb23af40875699cc9704988
Ana Cecilia <271367@unifio.edu.br>
2025-09-02 23:36:27 +0000

    aula0209

--- projeto-ana/src/app/produtos/estoque/estoque.ts
+++ projeto-ana/src/app/produtos/estoque/estoque.ts
@@ -2,27 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-estoque',
-  templateUrl: './estoque.component.html',
-  styleUrls: ['./estoque.component.css']
+  standalone: false,
+  templateUrl: './estoque.html',
+  styleUrl: './estoque.css'
 })
-export class EstoqueComponent {
+export class Estoque {
   produtos = [
     { nome: 'Notebook', preco: 3500, estoque: 5 },
     { nome: 'Teclado', preco: 150, estoque: 0 },
     { nome: 'Monitor', preco: 1200, estoque: 3 }
   ];
 
-  aumentarPrecos() {
-    this.produtos = this.produtos.map(p => ({
-      ...p,
-      preco: +(p.preco * 1.1).toFixed(2)   // aumenta 10%
-    }));
+  aumentarPreco() {
+    this.produtos.forEach(produto => {
+      produto.preco = produto.preco * 1.10;
+    })
   }
 
-  diminuirPrecos() {
-    this.produtos = this.produtos.map(p => ({
-      ...p,
-      preco: +(p.preco * 0.9).toFixed(2)   // diminui 10%
-    }));
+  diminuirPreco() {
+    this.produtos.forEach(produto => {
+      produto.preco = produto.preco * 0.90;
+    })
   }
+
 }
